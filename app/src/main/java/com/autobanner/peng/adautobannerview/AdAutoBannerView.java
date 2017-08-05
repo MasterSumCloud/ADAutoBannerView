736554748cbcb4acf00406ef4025440b48535cb6
Pengyan <summer.p.y@foxmail.com>
2017-08-05 15:36:31 +0800

    基本UIOK

--- app/src/main/java/com/autobanner/peng/adautobannerview/AdAutoBannerView.java
+++ app/src/main/java/com/autobanner/peng/adautobannerview/AdAutoBannerView.java
@@ -1,11 +1,11 @@
 package com.autobanner.peng.adautobannerview;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,38 +13,31 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created on 2017/8/4 16:26
  *
  * @author pengyan
- * @Des
+ * @Des 一款非常简单的自动滚动布局
  */
 
 public class AdAutoBannerView extends RelativeLayout {
 
     private Context mContext;
     private int mBottomMargin = 10;
-    private int mLeftMargin   = 20;
-    private int pointWith     = 24;
-    private int pointHigh     = 24;
-    private ArrayList<String> mImages;
-    private PagerAdapter      mPagerAdapter;
-    private LinearLayout      mLinearLayout;
+    private int mLeftMargin = 20;
+    private int pointWith = 24;
+    private LinearLayout mLinearLayout;
+    private ViewPager mViewPager;
 
 
     public AdAutoBannerView(Context context) {
         super(context, null);
-        Log.d("AdAutoBannerView", "11111111113");
         this.mContext = context;
         init();
     }
 
     public AdAutoBannerView(Context context, AttributeSet attrs) {
         super(context, attrs, 0);
-        Log.d("AdAutoBannerView", "11111111112");
         this.mContext = context;
         init();
     }
@@ -52,90 +45,96 @@ public class AdAutoBannerView extends RelativeLayout {
 
     public AdAutoBannerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        Log.d("AdAutoBannerView", "1111111111");
         this.mContext = context;
         init();
     }
 
     private void prepareViewpager() {
-        ViewPager viewPager = new ViewPager(mContext);
-        viewPager.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        mPagerAdapter = new PagerAdapter() {
-            @Override
-            public int getCount() {
-                return mImages.size();
-            }
-
-            @Override
-            public boolean isViewFromObject(View view, Object object) {
-                return view == object;
-            }
-
-            @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                ImageView imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new LinearLayout.LayoutParams(40,40));
-                imageView.setBackgroundColor(Color.parseColor(mImages.get(position)));
-                container.addView(imageView);
-                return imageView;
-            }
-        };
-        viewPager.setAdapter(mPagerAdapter);
-        this.setGravity(Gravity.BOTTOM);
-        this.addView(viewPager);
+        mViewPager = new ViewPager(mContext);
+        mViewPager.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        this.addView(mViewPager);
     }
 
 
-    public void init() {
-        if (mImages == null) {
-            mImages = new ArrayList<>();
-        }
+    private void init() {
         prepareViewpager();
-        setPoint();
     }
 
-
-    private void setPoint() {
+    /**
+     * 准备点的包裹布局
+     *
+     * @param count 点的数量
+     */
+    private void prepareLlPoint(int count) {
         mLinearLayout = new LinearLayout(mContext);
-        mLinearLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        mLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        mLinearLayout.setLayoutParams(layoutParams);
         mLinearLayout.setGravity(Gravity.CENTER);
-
-        addPoint();
+        addPoint(count);
     }
 
-    private void addPoint() {
+    /**
+     * 设置点
+     */
+    private void addPoint(int count) {
         mLinearLayout.removeAllViews();
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(pointWith, pointHigh);
-        ImageView point = new ImageView(mContext);
-        params.bottomMargin = mBottomMargin;
-        for (int i = 0; i < mImages.size(); i++) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        for (int i = 0; i < count; i++) {
+            ImageView point = new ImageView(mContext);
+            params.width = pointWith;
+            params.height = pointWith;
             if (i != 0) {
                 params.leftMargin = mLeftMargin;
                 point.setEnabled(false);
             }
             point.setLayoutParams(params);
             point.setBackgroundResource(R.drawable.rd_point_isselect);
-            ViewGroup parent = (ViewGroup) point.getParent();
-            if (parent != null) {
-                parent.removeAllViews();
-            }
             mLinearLayout.addView(point);
         }
-        ViewGroup parent = (ViewGroup) mLinearLayout.getParent();
-        if (parent != null) {
-            parent.removeAllViews();
-        }
+        setMargins(mLinearLayout, 0, 0, 0, mBottomMargin);
+        this.removeAllViews();
         this.addView(mLinearLayout);
     }
 
-    public void setData(List<String> imageList) {
-        if (mImages == null) {
-            mImages = new ArrayList<>();
-        }
-        if (imageList != null) {
-            mImages.addAll(imageList);
-        }
-        addPoint();
-        mPagerAdapter.notifyDataSetChanged();
+    /**
+     * 设置数据
+     */
+    public <T extends PagerAdapter> void setData(T adapter) {
+        mViewPager.setAdapter(adapter);
+        prepareLlPoint(adapter.getCount());
+        adapter.notifyDataSetChanged();
     }
+
+    private static void setMargins(View v, int l, int t, int r, int b) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(l, t, r, b);
+            v.requestLayout();
+        }
+    }
+
+    public void setPointWith(int newPintwith) {
+        if (newPintwith < 0) {
+            return;
+        }
+        this.pointWith = newPintwith;
+    }
+
+    public void setBottomMargin(int newBottomMargin) {
+        this.mBottomMargin = newBottomMargin;
+    }
+
+    public void setLayoutPointMargin(int newPointMargin) {
+        this.mLeftMargin = newPointMargin;
+
+    }
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
 }
--- app/src/main/java/com/autobanner/peng/adautobannerview/MainActivity.java
+++ app/src/main/java/com/autobanner/peng/adautobannerview/MainActivity.java
@@ -1,8 +1,12 @@
 package com.autobanner.peng.adautobannerview;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -12,29 +16,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("AdAutoBannerView", "..000000000");
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("AdAutoBannerView", "..22222222222");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("AdAutoBannerView", "..3333333333333");
         AdAutoBannerView viewById = (AdAutoBannerView) findViewById(R.id.aabv);
-        ArrayList<String> strings = new ArrayList<>();
+        final ArrayList<String> strings = new ArrayList<>();
         strings.add("#ff00ff");
         strings.add("#ffffff");
         strings.add("#ffeeff");
         strings.add("#ffee00");
         strings.add("#ffddff");
         strings.add("#00ffff");
-        viewById.setData(strings);
+        viewById.setData(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return strings.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ImageView imageView = new ImageView(MainActivity.this);
+                container.addView(imageView);
+                imageView.setBackgroundColor(Color.parseColor(strings.get(position)));
+                return imageView;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView((View) object);
+            }
+        });
 
     }
+
+
 }
